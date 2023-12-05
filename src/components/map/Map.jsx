b436991ca76c2c4e0d65c3fb4e7a148d98675fb7
tend <rydenko.urii@mail.ru>
2023-12-05 09:40:05 +0300

    some changes

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,28 +1,64 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { useEffect, useState } from 'react'
+const POLYLINE1 = {
+  id: 'one',
+  draggable: true,
+  geometry: {
+    type: 'LineString',
+    coordinates: [
+      // Specify the coordinates of the vertices of the polyline.
+      [37.5, 55.7],
+      [37.4, 55.8],
+      [37.5, 55.8],
+      [37.4, 55.7],
+    ],
+  },
+  style: { stroke: [{ color: '#f00', width: 4 }] },
+}
+
 
 export const ReactMap = () => {
   const [reactify, setReactify] = useState(null)
+  const [YMapZoomControl, setYMapZoomControl] = useState(null)
   useEffect(() => {
-    ymaps3
-      .import('@yandex/ymaps3-reactify')
-      .then(ymaps3Reactify => setReactify(ymaps3Reactify.reactify.bindTo(React, ReactDOM)))
+    Promise.all([
+      ymaps3.import('@yandex/ymaps3-reactify'),
+      ymaps3.import('@yandex/ymaps3-controls@0.0.1'),
+      ymaps3.ready,
+    ]).then(([ymaps3Reactify, yMapZoomControl]) => {
+      const temp = ymaps3Reactify.reactify.bindTo(React, ReactDOM)
+      setYMapZoomControl(temp.module(yMapZoomControl).YMapZoomControl)
+      setReactify(temp)
+    })
   }, [])
   if (!reactify) return <>Loading</>
-  const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } =
-    reactify.module(ymaps3)
+  const {
+    YMap,
+    YMapDefaultSchemeLayer,
+    YMapDefaultFeaturesLayer,
+    YMapMarker,
+    YMapFeature,
+    YMapCollection,
+    YMapControls,
+  } = reactify.module(ymaps3)
 
   return (
-    <YMap location={{ center: [37.623082, 55.75254], zoom: 9 }} mode='vector'>
+    <YMap location={{ center: [37.623082, 55.75254], zoom: 10 }} mode='vector'>
       <YMapDefaultSchemeLayer />
       <YMapDefaultFeaturesLayer />
-
-      {/* <YMapMarker coordinates={[25.229762, 55.289311]} draggable={true}>
-          <section>
-            <h1>You can drag this header</h1>
-          </section>
-        </YMapMarker> */}
+      <YMapCollection>
+        <YMapFeature {...POLYLINE1} />
+        <YMapFeature {...POLYLINE2} />
+      </YMapCollection>
+      <YMapControls position='right'>
+        <YMapZoomControl />
+      </YMapControls>
+      {/* <YMapMarker coordinates={[37.623082, 55.75254]} draggable={true}>
+        <section>
+          <h1>You can drag this header</h1>
+        </section>
+      </YMapMarker> */}
     </YMap>
   )
 }
